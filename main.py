d54a1c8017e0cee580bc8200a7a8034a04ec36dd
winnert-snek <joseph.winnert@gmail.com>
2022-04-26 15:00:51 -0500

    added comments for roadmap

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# GOAL: give input to user for name1 and name2 values
 
 def print_hi(name1, name2):
     # Use a breakpoint in the code line below to debug your script.
